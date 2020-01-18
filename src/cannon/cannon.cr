338c82098089b9e0db374b285e34eeaf0da9cd33
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2020-01-18 17:42:19 -0500

    output ppm from cannon

--- src/cannon/cannon.cr
+++ src/cannon/cannon.cr
@@ -2,22 +2,29 @@ require "../point"
 require "../vector"
 require "./projectile"
 require "./cannon_world"
+require "../canvas"
+require "../color"
 
 class Cannon
   def self.run
+    height = 550
+    canvas = Canvas.new(900, height)
     position = Point.new(0, 1, 0)
-    velocity = Vector.new(1.1, 1, 0)
+    velocity = Vector.new(1, 1.8, 0).normalize * 11.25
     projectile = Projectile.new(position, velocity)
 
     gravity = Vector.new(0, -0.1, 0)
     wind = Vector.new(-0.01, 0, 0)
     world = CannonWorld.new(gravity, wind)
+    red = Color.new(1, 0, 0)
 
     while projectile.position.y >= 0
       position = projectile.position
-      puts "x: #{position.x}, y: #{position.y}"
+      canvas.write_pixel(position.x.to_i, height - position.y.to_i, red)
       projectile = tick(world, projectile)
     end
+
+    print canvas.to_ppm
   end
 
   def self.tick(world, projectile)
@@ -27,5 +34,4 @@ class Cannon
   end
 end
 
-puts "running...."
 Cannon.run
