fca4ec4db142a213f25593f40ff999bf37199bbe
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2020-03-08 19:43:29 -0400

    add matrix multiplication

--- spec/matrix_spec.cr
+++ spec/matrix_spec.cr
@@ -2,10 +2,17 @@ require "./spec_helper"
 
 describe Matrix do
   describe ".new" do
-    it "accepts tuples" do
-      matrix = Matrix.new({1,2,3,4}, {5.5, 6.5, 7.5, 8.5}, {9, 10, 11, 12},{13.5, 14.5, 15.5, 16.5})
+    it "accepts arrays" do
+      matrix = Matrix.new(
+        [
+          [1, 2, 3, 4],
+          [5.5, 6.5, 7.5, 8.5],
+          [9.0, 10, 11, 12],
+          [13.5, 14.5, 15.5, 16.5],
+        ]
+      )
       matrix[0, 0]?.should eq 1
-      matrix[0, 3]?.should eq 4
+      matrix[0, 3]?.should eq 4.0
       matrix[1, 0]?.should eq 5.5
       matrix[1, 2]?.should eq 7.5
       matrix[2, 2]?.should eq 11
@@ -14,7 +21,7 @@ describe Matrix do
     end
 
     it "creates 2x2 matrix" do
-      matrix = Matrix.new({-3, 5},{1, -2})
+      matrix = Matrix.new([[-3, 5], [1, -2]])
       matrix[0, 0]?.should eq -3
       matrix[0, 1]?.should eq 5
       matrix[1, 0]?.should eq 1
@@ -22,7 +29,7 @@ describe Matrix do
     end
 
     it "creates 3x3 matrix" do
-      matrix = Matrix.new({-3, 5, 0},{1, -2, -7}, {0, 1, 1})
+      matrix = Matrix.new([[-3, 5, 0], [1, -2, -7], [0, 1, 1]])
       matrix[0, 0]?.should eq -3
       matrix[1, 1]?.should eq -2
       matrix[2, 2]?.should eq 1
@@ -31,53 +38,52 @@ describe Matrix do
 
   describe "equality between matrices" do
     it "when equal returns true" do
-      a = Matrix.new({1, 2, 3, 4},
-                     {5, 6, 7, 8},
-                     {9, 8, 7, 6},
-                     {5, 4, 3, 2})
+      a = Matrix.new([[1, 2, 3, 4],
+                      [5, 6, 7, 8],
+                      [9, 8, 7, 6],
+                      [5, 4, 3, 2]])
 
-      b = Matrix.new({1, 2, 3, 4},
-                     {5, 6, 7, 8},
-                     {9, 8, 7, 6},
-                     {5, 4, 3, 2})
+      b = Matrix.new([[1, 2, 3, 4],
+                      [5, 6, 7, 8],
+                      [9, 8, 7, 6],
+                      [5, 4, 3, 2]])
       a.should eq b
     end
 
     it "when different returns false" do
-      a = Matrix.new({1, 2, 3, 4},
-                     {5, 6, 7, 8},
-                     {9, 8, 7, 6},
-                     {5, 4, 3, 2})
+      a = Matrix.new([[1, 2, 3, 4],
+                      [5, 6, 7, 8],
+                      [9, 8, 7, 6],
+                      [5, 4, 3, 2]])
 
-      b = Matrix.new({2, 3, 4, 5},
-                     {6, 7, 8, 9},
-                     {8, 7, 6, 5},
-                     {4, 3, 2, 1})
+      b = Matrix.new([[2, 3, 4, 5],
+                      [6, 7, 8, 9],
+                      [8, 7, 6, 5],
+                      [4, 3, 2, 1]])
 
       a.should_not eq b
     end
   end
 
   describe "multiply 2 4x4 matrices" do
-   it "returns a new 4x4 matrix" do
-      a = Matrix.new({1, 2, 3, 4},
-                     {5, 6, 7, 8},
-                     {9, 8, 7, 6},
-                     {5, 4, 3, 2})
+    it "returns a new 4x4 matrix" do
+      a = Matrix.new([[1, 2, 3, 4],
+                      [5, 6, 7, 8],
+                      [9, 8, 7, 6],
+                      [5, 4, 3, 2]])
 
-      b = Matrix.new({-2, 1, 2, 3},
-                     {3, 2, 1, -1},
-                     {4, 3, 6, 5},
-                     {1, 2, 7, 8})
+      b = Matrix.new([[-2, 1, 2, 3],
+                      [3, 2, 1, -1],
+                      [4, 3, 6, 5],
+                      [1, 2, 7, 8]])
 
       c = a * b
 
-      d = Matrix.new({20, 22, 50, 48},
-                     {44, 54, 114, 108},
-                     {40, 58, 110, 102},
-                     {16, 26, 46, 42})
+      d = Matrix.new([[20, 22, 50, 48],
+                      [44, 54, 114, 108],
+                      [40, 58, 110, 102],
+                      [16, 26, 46, 42]])
       c.should eq d
-   end
+    end
   end
 end
-
--- src/matrix.cr
+++ src/matrix.cr
@@ -1,46 +1,41 @@
 class Matrix
-  getter :rows
-  getter :cols
   getter :data
-  @data :  Array(Array(Float64) | Array(Int32)) |  Array(Array(Int32))
-  def initialize(a, b, c, d)
-    @rows = 4
-    @cols = 4
-    @data = [
-      a.to_a,
-      b.to_a,
-      c.to_a,
-      d.to_a
-    ]
+  @data : Array(Array(Float64))
+
+  def initialize(nums)
+    @data = nums.map { |rows| rows.map { |col| col.to_f } }
   end
 
-  def initialize(a, b, c)
-    @rows = 3
-    @cols = 3
-    @data = [
-      a.to_a,
-      b.to_a,
-      c.to_a
-    ]
+  def []?(row : Int, col : Int) : Float64
+    @data[row][col]
   end
 
-  def initialize(a, b)
-    @rows = 2
-    @cols = 2
-    @data = [
-      a.to_a,
-      b.to_a
-    ]
+  def []=(row : Int32, col : Int32, data : Float64)
+    @data[row][col] = data.to_f
   end
 
-  def []? (col : Int, row : Int ) : Float64 | Int32 | Nil
-    @data[col][row]
+  def *(other : Matrix)
+    new_matrix = Matrix.new([[0, 0, 0, 0],
+                             [0, 0, 0, 0],
+                             [0, 0, 0, 0],
+                             [0, 0, 0, 0]])
+
+    4.times do |r|
+      4.times do |c|
+        cell = @data[r][0] * other.@data[0][c]
+        cell += @data[r][1] * other.@data[1][c]
+        cell += @data[r][2] * other.@data[2][c]
+        cell += @data[r][3] * other.@data[3][c]
+        new_matrix[r, c] = cell
+      end
+    end
+
+    new_matrix
   end
 
   def ==(other : Matrix)
-    false if self.rows != other.rows || self.cols != other.cols
     a = self.data.flatten
     b = other.data.flatten
-    b.zip(a).map{|x,y| (x-y).abs <= 0.00001}.all?
+    b.zip(a).map { |x, y| (x - y).abs <= 0.00001 }.all?
   end
 end
